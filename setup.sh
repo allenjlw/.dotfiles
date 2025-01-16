7cc67df542e563212fa9f597130a1a8577b3161f
wu <wu@rog>
2025-01-15 18:06:05 -0800

    update setup script

--- setup.sh
+++ setup.sh
@@ -28,6 +28,9 @@ ln -s `pwd`/.tigrc ~/.tigrc
 # config oh my zsh
 # install omz
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# install omz plugin
+cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-autosuggestions
+cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 # clone the configuration
 cd ~/.oh-my-zsh/custom && git clone https://github.com/allenjlw/terminal-conf.git
 ln -s ~/.oh-my-zsh/custom/terminal-conf/custom-bira.zsh-theme ~/.oh-my-zsh/custom/custom-bira.zsh-theme
